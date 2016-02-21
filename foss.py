6da263f9a0dcdd8e527e6577ee1c79406e2d32e2
Vivek Seth <Vivekseth.m@gmail.com>
2016-02-21 15:30:49 -0500

    init

---
+++ foss.py
@@ -0,0 +1,116 @@
+import random
+import string
+import requests
+import secrets
+from flask import Flask, request, make_response, render_template
+
+## Globals
+
+app = Flask(__name__)
+
+## Helper Functions
+
+def random_state():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+def authorization_url(client_id, redirect_uri, scope, state):
+    url = 'https://github.com/login/oauth/authorize?client_id='+client_id + \
+        '&redirect_uri='+redirect_uri+ \
+        '&scope='+scope + \
+        '&state='+state
+    return url
+
+def temp_code_to_access_code(client_id, client_secret, code, redirect_uri, state):
+    url = 'https://github.com/login/oauth/access_token'
+    headers = {'Accept': 'application/json'}
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'state': state
+    }
+    r = requests.post(url, headers=headers, data=data)
+    return r
+
+def repo_stat_endpoint(repo_path):
+	endpoint = '/repos/' + repo_path + '/stats/code_frequency'
+	return endpoint
+
+def api_request(token, endpoint):
+	headers = {
+		'Accept': 'application/vnd.github.v3+json',
+		'Authorization': 'token ' + token
+	}
+	base_url = 'https://api.github.com'
+	r = requests.get(base_url + endpoint, headers=headers)
+	return r
+
+def repo_stat(json):
+	total_additions = reduce(lambda x, y: x+y, map(lambda x: x[1], json), 0)
+
+	char_per_addition = 40 # (1/2) * 80 char per line
+	char_per_word = 5 # https://en.wikipedia.org/wiki/Words_per_minute
+	wpm_comp = 19 # https://en.wikipedia.org/wiki/Words_per_minute
+	salary_per_hour = 45.187 # https://www.glassdoor.com/Salaries/software-engineer-salary-SRCH_KO0,17.htm
+
+	total_hours = (total_additions * char_per_addition) / (wpm_comp * char_per_word * 60)
+	total_compensation = salary_per_hour * total_hours
+
+	data = {'url': repo_url, 'hours': total_hours,'cost': total_compensation}
+	return data
+
+## Routes
+
+@app.route('/', methods=['GET'])
+def main_page():
+	access_token = request.cookies.get('access_token')
+	if not access_token:
+		state = random_state()
+		resp = make_response(render_template('./login.html', auth_url=authorization_url(secrets.CLIENT_ID, secrets.REDIRECT_URI, secrets.SCOPE, state)))
+		resp.set_cookie('state', state)
+		return resp
+	else:
+		resp = make_response(render_template('./form.html'))
+		return resp
+
+@app.route('/', methods=['POST'])
+def result():
+	repo_url = request.form.get('repo')
+	try:
+		cleaned_repo = '/'.join(filter(lambda x: len(x) > 0, repo_url.split('/'))[-2:])
+		access_token = request.cookies.get('access_token')
+		r = api_request(access_token, repo_stat_endpoint(cleaned_repo))
+		data = repo_stat(r.json())
+		if data['cost'] < 1:
+			return 'Please try again by refreshing this page.'
+		else:
+			return make_response(render_template('./form.html', data=data))
+	except Exception, e:
+		print e
+		return 'error'
+
+
+@app.route('/auth_code', methods=['GET'])
+def index():
+	state = request.cookies.get('state')
+	code = request.args.get('code')
+
+	r = temp_code_to_access_code(secrets.CLIENT_ID, secrets.CLIENT_SECRET, code, secrets.REDIRECT_URI, state)
+	try:
+		j = r.json()
+		access_token = j['access_token']
+		resp = make_response(render_template('./form.html'))
+		resp.set_cookie('access_token', access_token)
+		return resp
+	except Exception, e:
+		print e
+		print r.text
+		return 'ERROR'
+
+
+## Main
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run(host="0.0.0.0", port=5100)
